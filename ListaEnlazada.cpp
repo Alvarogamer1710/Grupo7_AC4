781e758a3624cf71d7c641ac84d63d470b486426
diablometal <114223491+diablometal@users.noreply.github.com>
2025-12-05 19:05:11 +0100

    Implement Linked List for Contact Management

---
+++ ListaEnlazada.cpp
@@ -0,0 +1,67 @@
+
+#include <string>class ListaContactos {
+    protected:
+    class Nodo {
+        std::string nombre;
+        int edad;
+        Nodo* nexo;
+        Nodo* prev;
+    public:
+        Nodo(){
+            this->nombre = "";
+            this->edad = 0;
+            this->nexo = nullptr;
+            this->prev = nullptr;
+        };        Nodo(int edad, std::string nombre){
+            this->nombre = "";
+            this->edad = 0;
+            this->nexo = nullptr;
+            this->prev = nullptr;
+        };        Nodo(int dato, Nodo* nexo, Nodo* prev){
+            this->nombre = "";
+            this->edad = 0;
+            this->nexo = nexo;
+            this->prev = prev;
+
+        };    public:        void cambiarLast(Nodo* prev) {
+            this->prev = prev;
+        }
+
+        void cambiarNexo(Nodo* nexo) {
+            this->nexo = nexo;
+        }
+        Nodo* getNext() {
+            return this->nexo;
+        }
+        Nodo* getPrev() {
+            return this->prev;
+        }
+
+        std::string getName() {
+            return this->nombre;
+        }
+        int getEdad() {
+            return this->edad;
+        }
+
+        void setEdad(int edad) {
+            this->edad = edad;
+        }
+
+        void setName(std::string nombre) {
+            this->nombre = nombre;
+        }
+    };
+private:
+    Nodo* first;
+    Nodo* last;
+
+public:
+    ListaContactos() {
+        first = nullptr;
+        last = nullptr;
+
+    }
+
+
+};
